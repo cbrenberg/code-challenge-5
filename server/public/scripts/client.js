6e7986c3877aa59587c72e2a184cd1f88f14bc19
Christopher Brenberg <cbrenberg@gmail.com>
2018-10-01 10:45:38 -0500

    index.html input connected to angular controller for posting and displaying messages

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,36 +2,33 @@ console.log('client.js loaded');
 
 const app = angular.module('MessageBoardApp', [])
 
-app.controller('MessagesController', ['$http', function($http) {
+app.controller('MessagesController', ['$http', function ($http) {
   let vm = this;
-  vm.messageToAdd={
-    name: 'Luke',
-    message: 'Do your feedback'
-  };
+  vm.messageToAdd = {};
+  vm.messages = [];
 
   vm.getMessages = function () {
     $http.get('/messages')
-      .then(function(response) {
+      .then(function (response) {
         console.log('Messages have arrived!', response.data);
+        vm.messages = response.data;
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log('Error getting messages', error);
       })
   }//end getMessages
 
   vm.postMessage = function () {
     $http.post('/messages', vm.messageToAdd)
-      .then(function(response) {
-        console.log('Message added', response.data);
+      .then(function (response) {
+        console.log('Message added', response);
+        vm.getMessages();
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log('Error posting message', error);
       })
   }
-  vm.postMessage();
-//get Message list on page load
-vm.getMessages();
-
-
+  //get Message list on page load
+  vm.getMessages();
 
 }])//end controller
